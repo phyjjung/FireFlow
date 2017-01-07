33a7c715811cd1ac0a3abdb78130374ee3edb892
phyjjung <junghoon.bak@gmail.com>
2017-01-07 10:58:41 +0900

    1차 flowlist완성
    Auth.uid를 만들어야함

--- www/components/flow/flowlist.controller.js
+++ www/components/flow/flowlist.controller.js
@@ -1,27 +1,14 @@
 angular.module('starter')
 
-  .controller('FlowCtrl', function($scope,FlowListService, PostService, $stateParams,flowlist,userflowlist) {
-    $scope.flowlist = flowlist;
-    console.log($scope.flowlist);
-$scope.userflowlist = userflowlist;
-console.log($scope.userflowlist);
+  .controller('FlowCtrl', function($scope,FlowListService, $stateParams,userflowlist) {
+    $scope.flowlist2 = userflowlist;
+    console.log($scope.flowlist2);
+
     $scope.getData = function() {
       FlowListService.query()
         .then(function(response) {
-          $scope.results = $scope.flowlist;
+          $scope.results = $scope.flowlist2;
           //  $scope.results = response; 원래 이렇게 되어있었음.. response는 servce에서 정의되어있던 data임. 거기다 flowlist를 붙혀줌
-          // Stop the ion-refresher from spinning
-          $scope.$broadcast('scroll.refreshComplete');
-        })
-    }
-
-    if ($stateParams.q) {
-      $scope.currentQuery = $stateParams.q;
-
-      PostService.getAll()
-        .then(function(response) {
-          $scope.posts = response;
-
           // Stop the ion-refresher from spinning
           $scope.$broadcast('scroll.refreshComplete');
         })
--- www/components/flow/flowlist.service.js
+++ www/components/flow/flowlist.service.js
@@ -20,6 +20,7 @@ angular.module('starter')
   })
 
   .factory('flowlist', ['$firebaseArray','$firebaseObject',
+
   function($firebaseArray, $firebaseObject,Auth){
     var flowlistRef = firebase.database().ref().child("flow").child("flowlist").orderByChild("followers");
     console.log("FlowList factory");
@@ -29,29 +30,44 @@ angular.module('starter')
     return FlowList;
   }])
 
-.factory('userflowlist',['$firebaseArray', '$firebaseObject',
-  function($firebaseArray,Auth){
+.service('userflowlist',['$firebaseArray', '$firebaseObject',
+
+  function($firebaseArray,$firebaseObject,Auth){
+
     const rootRef = firebase.database().ref();
     const userkey ='NyRMDzW79ncYvIgu5qvyBbzKmv93';
     const flowlistRef = rootRef.child("flow").child("flowlist");
-    var FlowList = $firebaseArray(flowlistRef);
+
     console.log('userflowlist');
     //유저의 flowlist를 가지고옴
    const userflow = rootRef.child("users").child(userkey);
-    //userflow.child('UserFlowList').once ('value',snap =>{console.log(snap.val());console.log (snap.key)});
+
 
     // userlist에서 받은걸로 flow셜과 가지고옴.
-    function getFlowListProfileForUser (userkey,cb){
+    function getFlowListProfileForUser (userkey){
+
+        var flowlist = new Array();
         userflow.child('UserFlowList').on('child_added', snap =>{
+          //snap안에 쓰잘데기 없는게 있으면서 그 사이에 key가 있음
           let flowlistForUser = flowlistRef.child (snap.key);
-
-          flowlistForUser.once('value',cb);
+          //그 key 위치를 이용하여 받아옴.
+          var FlowListtest = $firebaseObject(flowlistForUser);
+          flowlist.push(FlowListtest);
+          //보통은 아래와 같이 once로 받아옴. 하지만 여기서는 firebaseObject로 받아서 array push
+          // flowlistForUser.once('value').then((snap) => {
+          //  flowlist.push(snap.val());
+          //  console.log (snap.val());
+        //  });
+          // flowlistForUser.once('value',cb);
         });
-    }
 
-    getFlowListProfileForUser (userkey,snap => console.log (snap.val()));
+        return flowlist;
+    };
 
-return FlowList;
+    var userlistfinal = getFlowListProfileForUser (userkey);
+
+
+return userlistfinal
 
   }
 ])
