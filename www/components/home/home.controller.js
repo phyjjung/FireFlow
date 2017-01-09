cca4a07c653de93e1dcdc8bb48fd618e05884356
phyjjung <junghoon.bak@gmail.com>
2017-01-09 23:02:43 +0900

    floating button 조금 수정
    new tweet내에 에러를 수정필요

--- www/components/home/home.controller.js
+++ www/components/home/home.controller.js
@@ -1,6 +1,6 @@
 angular.module('starter')
 
-  .controller('HomeCtrl', function($scope, PostService) {
+  .controller('HomeCtrl', function($scope, $ionicModal, PostService) {
     $scope.getData = function() {
       PostService.getAll()
         .then(function(response) {
@@ -13,5 +13,51 @@ angular.module('starter')
 
     //////////////////
     $scope.getData();
+
+
+///new_tweet modal 시작
+    $ionicModal.fromTemplateUrl('components/tweet/views/new_tweet.modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+
+    $scope.open_new_tweet = function() {
+      $scope.modal.show();
+    };
+    $scope.close = function() {
+      $scope.modal.hide();
+    };
+
+    $scope.addGif = function() {
+      Giphy.openModal()
+        .then(function(imageUrl) {
+          $scope.tweet.imgSrc = imageUrl;
+        })
+    }
+
+    $scope.addPhoto = function() {
+      PhotoService.add()
+        .then(function(imageData) {
+          $scope.tweet.imgSrc = imageData;
+        })
+    }
+
+    $scope.addLocation = function() {
+      LocationService.add()
+        .then(function(location) {
+
+        })
+    }
+
+    $scope.send = function() {
+      $scope.close();
+    }
+///new_tweet modal 마지막
+
+
+
+
+
   });
-  
