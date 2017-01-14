bd0f45d036a660d2cac50ae7e6b08f9ab9981f45
phyjjung <junghoon.bak@gmail.com>
2017-01-14 23:03:37 +0900

    flowList 만드는중
    form에서 받은게 날라가지 않음..
    동영상 봐야겠음

--- www/components/flow/flowlist.controller.js
+++ www/components/flow/flowlist.controller.js
@@ -1,49 +1,36 @@
 angular.module('starter')
 
-  .controller('FlowCtrl', function($scope,$ionicModal,userflowlist,Auth) {
+.controller('FlowCtrl', function($scope,$ionicModal,userflowlist,Auth) {
+
   $authuid = Auth.$getAuth().uid;
   $scope.flowlist=userflowlist.getflowlist($authuid) ;
   //flowlist를 애귤라에 연결
   $scope.results = $scope.flowlist;
 
   ///new_flow modal 시작
-      $ionicModal.fromTemplateUrl('components/flow/views/new_flow.modal.html', {
-        scope: $scope,
-        animation: 'slide-in-up'
-      }).then(function(modal) {
-        $scope.modal = modal;
-      });
-
-      $scope.open_new_flow = function() {
-        $scope.modal.show();
-      };
-      $scope.close = function() {
-        $scope.modal.hide();
-      };
-
-      $scope.addGif = function() {
-        Giphy.openModal()
-          .then(function(imageUrl) {
-            $scope.tweet.imgSrc = imageUrl;
-          })
-      }
-
-      $scope.addPhoto = function() {
-        PhotoService.add()
-          .then(function(imageData) {
-            $scope.tweet.imgSrc = imageData;
-          })
-      }
-
-      $scope.addLocation = function() {
-        LocationService.add()
-          .then(function(location) {
-
-          })
-      }
-
-      $scope.send = function() {
-        $scope.close();
-      }
-  ///new_flow Modal modal 마지막
+  $ionicModal.fromTemplateUrl('components/flow/views/new_flow.modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
   });
+
+  $scope.open_new_flow = function() {
+    $scope.modal.show();
+  };
+  $scope.close = function() {
+    $scope.modal.hide();
+  };
+
+
+$scope.newFlowName ="sdf";
+console.log("outfunciton : " + $scope.newFlowName);
+
+  $scope.send = function() {
+    var flowname = $scope.newFlowName;
+    console.log("infunciton : "+ flowname);
+    userflowlist.makeNewFlow($authuid,flowname);
+    $scope.close();
+  }
+  ///new_flow Modal modal 마지막
+});
--- www/components/flow/flowlist.service.js
+++ www/components/flow/flowlist.service.js
@@ -2,29 +2,55 @@ angular.module('starter')
 
 .service('userflowlist',['$firebaseArray', '$firebaseObject',function($firebaseArray,$firebaseObject){
 
-    // userlist에서 받은걸로 flow셜과 가지고옴.
-    this.getflowlist= function (userid){
-      const rootRef = firebase.database().ref();
-      const flowlistRef = rootRef.child("flow").child("flowlist");
-     const userflow = rootRef.child("users").child(userid);
+  const rootRef = firebase.database().ref();
+  const flowlistRef = rootRef.child("flow").child("flowlist");
+  // userlist에서 받은걸로 flow셜과 가지고옴.
+  this.getflowlist= function (userid){
 
-        var flowlist = new Array();
-        userflow.child('UserFlowList').on('child_added', snap =>{
-            //snap안에 쓰잘데기 없는게 있으면서 그 사이에 key가 있음
-            let flowlistForUser = flowlistRef.child (snap.key);
-            // 그 key 위치를 이용하여 받아옴.
-            //유저의 flowlist를 가지고옴
-            var FlowListtest = $firebaseObject(flowlistForUser);
-            flowlist.push(FlowListtest);
-        });
+    const userflow = rootRef.child("users").child(userid);
 
-        return flowlist;
+    var flowlist = new Array();
+    userflow.child('UserFlowList').on('child_added', snap =>{
+      //snap안에 쓰잘데기 없는게 있으면서 그 사이에 key가 있음
+      let flowlistForUser = flowlistRef.child (snap.key);
+      // 그 key 위치를 이용하여 받아옴.
+      //유저의 flowlist를 가지고옴
+      var FlowListtest = $firebaseObject(flowlistForUser);
+      flowlist.push(FlowListtest);
+    });
+
+    return flowlist;
+  };
+
+
+this.makeNewFlow= function (userid,flowName,descrpition){
+  var flowData = {
+      author: "",
+      title: "",
+      followers : 1,
+      description : ""
     };
 
+ flowData.author = userid;
+ flowData.title = flowName;
 
-return this;
+ console.log("makeNewFlow  :"+flowName);
+ const userflow = rootRef.child("users").child(userid).child('UserFlowList');
+ //key를 만드는 동시에 받음.. 'true'가 내용이 되면서 들어감.
+ var newPostKey =  userflow.push('true').key;
+ var makeFlow = {};
 
-  }
+
+ makeFlow['/flow/flowlist/' + newPostKey] = flowData;
+
+ firebase.database().ref().update(makeFlow);
+
+}
+
+
+  return this;
+
+}
 ])
 
 //  .factory('flowlist', ['$firebaseArray','$firebaseObject',
