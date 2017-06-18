787d77541d62fb928274deb921818b18770aa090
오재욱 <ssibaralralra@gmail.com>
2017-06-19 02:05:32 +0900

    post 저장하기 post 불러오기 저장하고 불러올 때 에러처리 해야함

--- www/components/flow/flowlist.service.js
+++ www/components/flow/flowlist.service.js
@@ -121,7 +121,6 @@ function($firebaseArray,$firebaseObject,$q){
 
   }
 
-
   return this;
 
 }
--- www/components/flow/flowpost.service.js
+++ www/components/flow/flowpost.service.js
@@ -1,32 +1,52 @@
 angular.module('starter')
-  //  만드는 중
+//  만드는 중
   .service('FlowPostService', ["$firebaseArray",
-  function($firebaseArray) {
-    const ref = firebase.database().ref();
-    const flowForPostRef = ref.child("post").child("postlistforFlow");
+    function ($firebaseArray) {
+      const ref = firebase.database().ref();
+      const flowForPostRef = ref.child("post").child("postlistforFlow");
 
-    this.getAll = function(flowId){
-
-         var AllPostForFlow = flowForPostRef.child("-KeXBvhexESwhaws4ton");
-       var query = AllPostForFlow.orderByChild("created_at").limitToLast(25);
-       console.log($firebaseArray(query));
-       return $firebaseArray(query)
+      this.getAll = function (flowId) {
+        console.log("getAll 시작");
+        var AllPostForFlow = flowForPostRef.child(flowId);
+        var query = AllPostForFlow.orderByChild("created_at").limitToLast(25);
+        console.log($firebaseArray(query));
+        return $firebaseArray(query)
         // type: {
         //   name: 'reply',
         //   referencer: 'Hieu Pham'
         // }
       };
 
-      /* Tweet isReference */
-      // 0: normal
-      // 1: true
+      this.makeNewPost = function(userId, postTitle, content, flowId) {
+        console.log("makeNewPost 시작");
+        const rootRef = firebase.database().ref();
+        const newPostKey = rootRef.child('post').child('postlist').push().key;
 
-      /* Tweet TYPE */
-      // 0: normal
-      // 1: photo
-      // 2: link
-      // 3: repos
-      // 4: article
+        console.log("new post key ="+newPostKey);
+        console.log("author id="+userId);
+        var postData = {
+          author: "",
+          title: "",
+          text: "",
+          flow: "",
+          createdAt: firebase.database.ServerValue.TIMESTAMP
+        };
 
+        postData.author = userId;
+        postData.title = postTitle;
+        postData.text = content;
+        postData.flow = flowId;
 
-  }]);
+        var makePost = {};
+        makePost['/post/postlist/' + newPostKey] = postData;
+        makePost['/post/postlistforFlow/' + flowId + '/' + newPostKey] = postData;
+        makePost['/users/' + userId + '/userpostlist/' + newPostKey] = postData;
+
+        rootRef.update(makePost);
+        //에러처리 추가 해야됨
+        console.log("makeNewPost 끝");
+      };
+
+      return this;
+    }
+  ]);
