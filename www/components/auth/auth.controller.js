f7ab067723441cffe47834dcf552c11219b5a9ab
phyjjung <junghoon.bak@gmail.com>
2017-06-25 22:54:40 +0900

    google email추가

--- www/components/auth/auth.controller.js
+++ www/components/auth/auth.controller.js
@@ -27,6 +27,30 @@ angular.module('starter')
       $scope.signupModal.show();
     }
 
+    $scope.GoogleSignup = function(){
+      console.log("GoogleSignup");
+  var provider = new firebase.auth.GoogleAuthProvider();
+  provider.addScope('https://www.googleapis.com/auth/plus.login');
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+  $state.go('tab.home');
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+    }
+
     $scope.closeLogin = function () {
       $scope.loginModal.hide();
     };
